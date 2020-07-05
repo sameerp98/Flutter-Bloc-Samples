650e9d76723382744b36d0f2bfb51a7036c8281b
Sameer Padalia <sameerp98@pop-os.localdomain>
2020-07-06 04:49:59 +0530

    added Timer widget

---
+++ lib/widgets/timer.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_timer/bloc/timer_bloc.dart';
+
+class Timer extends StatelessWidget {
+  static const TextStyle timerTextStyle = TextStyle(
+    fontSize: 60,
+    fontWeight: FontWeight.bold,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Timer'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 100.0),
+            child: Center(
+              child: BlocBuilder<TimerBloc, TimerState>(
+                builder: (context, state) {
+                  final String minuteStr = ((state.duration / 60) % 60)
+                      .floor()
+                      .toString()
+                      .padLeft(2, '0');
+                  final String secondsStr =
+                      (state.duration % 60).floor().toString().padLeft(2, '0');
+                  return Text('$minuteStr:$secondsStr',
+                      style: Timer.timerTextStyle);
+                },
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
