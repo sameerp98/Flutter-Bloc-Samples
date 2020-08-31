e83d82f46f298e2b36493a97d15ba47f11cd7ef4
Sameer Padalia <sameerp98@pop-os.localdomain>
2020-08-31 16:31:06 +0530

    added state transition logging

--- flutter_weather/lib/main.dart
+++ flutter_weather/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import './simple_bloc_delegate.dart';
 
 void main() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
   runApp(MyApp());
 }
 
